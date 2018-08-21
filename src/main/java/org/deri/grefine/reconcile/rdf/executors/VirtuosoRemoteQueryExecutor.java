6c82daec738297cef52042a6d0c7e6b5c8563d6d
Stuart Kenny <skenny@tchpc.tcd.ie>
2018-08-21 21:04:42 +0100

    Close query execution objects to release resources and prevent lock-ups

--- src/main/java/org/deri/grefine/reconcile/rdf/executors/VirtuosoRemoteQueryExecutor.java
+++ src/main/java/org/deri/grefine/reconcile/rdf/executors/VirtuosoRemoteQueryExecutor.java
@@ -2,37 +2,49 @@ package org.deri.grefine.reconcile.rdf.executors;
 
 import java.util.Collections;
 
+import org.apache.jena.query.ResultSetFactory;
 import org.json.JSONException;
 import org.json.JSONWriter;
 
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 
-public class VirtuosoRemoteQueryExecutor extends RemoteQueryExecutor{
+public class VirtuosoRemoteQueryExecutor extends RemoteQueryExecutor {
 
-	public VirtuosoRemoteQueryExecutor(String sparqlEndpointUrl,String defaultGraphUri) {
-		super(sparqlEndpointUrl,defaultGraphUri);
-	}
+    public VirtuosoRemoteQueryExecutor(String sparqlEndpointUrl, String defaultGraphUri) {
+        super(sparqlEndpointUrl, defaultGraphUri);
+    }
 
-	@Override
-	public ResultSet sparql(String sparql) {
-		//we use QueryEngineHTTP to skip query validation as Virtuoso needs non-standardised extensions and will not pass ARQ validation
-		QueryEngineHTTP qExec = new QueryEngineHTTP(sparqlEndpointUrl, sparql);
-		if(defaultGraphUri!=null){
-			qExec.setDefaultGraphURIs(Collections.singletonList(defaultGraphUri));
-		}
-		ResultSet res = qExec.execSelect();
-		return res;
-	}
-	
-	@Override
-	public void write(JSONWriter writer) throws JSONException {
-		writer.object();
-		writer.key("type");	writer.value("remote-virtuoso");
-		writer.key("sparql-url"); writer.value(sparqlEndpointUrl);
-		if(defaultGraphUri!=null){
-			writer.key("default-graph-uri"); writer.value(defaultGraphUri);
-		}
-		writer.endObject();
-	}
+    @Override
+    public ResultSet sparql(String sparql) {
+        // we use QueryEngineHTTP to skip query validation as Virtuoso
+        // needs non-standardised extensions and will not pass ARQ validation
+        QueryEngineHTTP qExec = new QueryEngineHTTP(sparqlEndpointUrl, sparql);
+        if (defaultGraphUri != null) {
+            qExec.setDefaultGraphURIs(Collections.singletonList(defaultGraphUri));
+        }
+        ResultSet results = null;
+        try {
+            ResultSet res = qExec.execSelect();
+            results = ResultSetFactory.copyResults(res);
+        } finally {
+            qExec.close();
+        }
+
+        return results;
+    }
+
+    @Override
+    public void write(JSONWriter writer) throws JSONException {
+        writer.object();
+        writer.key("type");
+        writer.value("remote-virtuoso");
+        writer.key("sparql-url");
+        writer.value(sparqlEndpointUrl);
+        if (defaultGraphUri != null) {
+            writer.key("default-graph-uri");
+            writer.value(defaultGraphUri);
+        }
+        writer.endObject();
+    }
 }
